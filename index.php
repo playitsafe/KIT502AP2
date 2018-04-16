039740fb801a18f662c17a5b04f88db9c20e6efe
aaron <playitsafe@qq.com>
2018-04-16 22:24:52 +1000

    add header for index

---
+++ index.php
@@ -0,0 +1,64 @@
+ <?php
+    include 'header.php';
+ ?>  
+    
+    <div id="mainarea">
+                
+        <div id="shopcontainer">
+            <ul>
+                <li class="shops">
+                    <a href="index.html">
+                        <div class="shopimg">
+                            <img src="img/lzby.jpg">
+                        </div>
+                        <div class="shopname">
+                            The Lazenbys'
+                        </div>
+                    </a>
+                </li>
+                <li class="shops">
+                    <a href="index.html">
+                        <div class="shopimg"><img src="img/ref.jpg">
+                        </div>
+                        <div class="shopname">
+                        The Ref
+                        </div>
+                    </a>
+                </li>
+                <li class="shops">
+                    <a href="index.html">
+                        <div class="shopimg"><img src="img/ttt.jpg">
+                        </div>
+                        <div class="shopname">
+                        The Trade Table
+                        </div>
+                    </a>
+                </li>
+                
+            </ul>
+            
+        </div>
+       
+    </div>
+    
+    <script>
+        
+        
+        $("#loginbox").hide();
+        window.onclick = function(event){
+            if($(event.target).closest("#loginbtn").length){
+                $("#loginbox").show();                                
+            } else if($(event.target).closest("#loginbox").length){
+                $("#loginbox").show();
+            } else {
+                $("#loginbox").hide();
+            } ;      
+        };
+        
+    </script>
+    
+    
+    
+    
+</body>
+</html>
